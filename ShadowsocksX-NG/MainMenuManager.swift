78d77e65f4ec8fec7e3bb94606b77e314ca1043f
zack <shiqizhenyes@gmail.com>
2021-01-23 23:03:59 +0800

    添加配置
    添加配置

--- ShadowsocksX-NG/MainMenuManager.swift
+++ ShadowsocksX-NG/MainMenuManager.swift
@@ -537,6 +537,8 @@ class MainMenuManager: NSObject, NSUserNotificationCenterDelegate {
         } else {
             statusItem.image = NSImage(named: "menu_icon_disabled")!
         }
+//        print("zack updateStatusItemUI")
+//        print(statusItem.highlightMode)
         statusItem.image?.isTemplate = true
     }
     
